a5cb1798b2220d53decf165af64a4e76c1ae2c79
Alexandra Franzen <alexandra.s.franzen@gmail.com>
2024-05-19 22:03:21 +0200

    Created folder structure and installed styled components

---
+++ my-react-portfolio/src/App.jsx
@@ -0,0 +1,11 @@
+import { Introduction } from "./components/1.Introduction.jsx";
+
+export const App = () => {
+  return (
+    <>
+      <Introduction />
+    </>
+  );
+};
+
+export default App;
---
+++ my-react-portfolio/src/components/1.Introduction.jsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const Introduction = () => {
+  return (
+    <div>
+      <section>
+        <h1>Hi, I'm Alexandra Franzén</h1>
+        <h2>Frontend Developer</h2>
+        <p>This will be my presentation text</p>
+      </section>
+    </div>
+  );
+};
+{
+  /* <img src={}
+alt="Profile picture"/> */
+}
+
+styled.h1`
+  color: #000;
+  font-family: Montserrat;
+  font-size: 32px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+`;
