87a9cec50486e8502ccd28aa158269eef969622e
Alexandra Franzen <alexandra.s.franzen@gmail.com>
2024-11-28 11:31:22 +0100

    added footer and links

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Introduction } from "./components/1.Introduction.jsx";
 import { Tech } from "./components/2.Tech.jsx";
 import { Projects } from "./components/3.Projects.jsx";
+import { Footer } from "./components/7.Footer.jsx"
 
 export const App = () => {
   return (
@@ -8,6 +9,7 @@ export const App = () => {
       <Introduction />
       <Tech />
       <Projects />
+      <Footer />
     </>
   );
 };
