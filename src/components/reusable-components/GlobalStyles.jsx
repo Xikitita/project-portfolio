8227e8e93f2a1d234183c977d00ebaff38063d9d
Alexandra Franzen <alexandra.s.franzen@gmail.com>
2024-12-09 14:07:34 +0100

    added global body background-color

--- src/components/reusable-components/GlobalStyles.jsx
+++ src/components/reusable-components/GlobalStyles.jsx
@@ -2,6 +2,7 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
 body {
+background-color: #FFFFFF;
   margin: 0;
   padding: 0;
   overflow-x: hidden;
